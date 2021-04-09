ac23544a0094294ed04ab07e0a417944d5e28790
Aleksey Belchenkov <u608110@gmail.com>
2021-04-09 10:30:33 +0400

    Create CRUD for Product

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/products', function () {
-   return 'products';
-});
+Route::resource('products', ProductController::class);
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
