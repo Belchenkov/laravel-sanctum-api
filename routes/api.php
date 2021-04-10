650d87bcced8f3143da1756bd8286e858c4ae45b
Aleksey Belchenkov <u608110@gmail.com>
2021-04-10 20:30:21 +0400

    Protect Route

--- routes/api.php
+++ routes/api.php
@@ -15,9 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('products', ProductController::class);
+
 Route::get('/products/search/{name}', [ProductController::class, 'search']);
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::resource('products', ProductController::class);
 });
